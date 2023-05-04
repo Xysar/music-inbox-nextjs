2f3f6eba480cfcdb918e50307d50f6e5bd765154
Cesar Camacho <cesarcamacho760@gmail.com>
2023-05-04 15:15:23 -0700

    remove border from tracklist component

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -64,20 +64,20 @@ const AlbumCard = ({ currentAlbum, currentAlbumId }: any) => {
             ></p>
             <div
               onClick={() => setShowTracklist((prev) => !prev)}
-              className="relative box-border w-full cursor-pointer border-2 border-gray-600 bg-black p-2 "
+              className="relative box-border w-full cursor-pointer  border-gray-600 bg-black p-2 "
             >
               {returnTracklist()}
               <div
                 className={`${
                   showTracklist ? "block" : "hidden"
-                } absolute left-0 top-0 h-44 w-[100%] overflow-scroll overflow-x-hidden  `}
+                } absolute left-0 top-0 h-44 w-[100%] overflow-scroll `}
               >
                 {currentAlbum?.tracks?.track.map(
                   (curTrack: any, index: number) => {
                     return (
                       <div
                         key={index}
-                        className="flex w-full justify-between border-2 border-gray-600 bg-black p-2 "
+                        className="flex w-full justify-between  border-gray-600 bg-black p-2 "
                       >
                         <div className="flex gap-2">
                           <p>{index + 1}.</p>
